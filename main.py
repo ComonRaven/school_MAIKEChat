6de21df1ac3ed51b19fb7a6b15cf33b5cc09bb64
ComonRaven <moonandstar118@gmail.com>
2024-12-22 17:01:12 +0900

    openAI APIで表現

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import eel
-import requests
+import functions
 
 # Eelの初期化
 eel.init("web")
-
-@eel.expose
-def get_generated_code(input_text):
-    # Flask APIにPOSTリクエストを送信
-    response = requests.post("http://localhost:5000/process_input", json={"input_text": input_text})
-
-    print("Response Text:", response.text)  # レスポンスの内容を確認
-    return response.json().get("generated_code", "Error")
-
-if __name__ == "__main__":
-    eel.start("AI.html", mode="default")  # フロントエンドUIの起動
+eel.start("AI.html", mode="default")  # フロントエンドUIの起動
