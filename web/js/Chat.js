e0fe864f54585d371b6e8dee3ea28e7c5543a41b
ComonRaven <moonandstar118@gmail.com>
2024-12-23 00:07:25 +0900

    paizaと接続して対応するwebコードエディタを表示

--- web/js/Chat.js
+++ web/js/Chat.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
       let question = document.getElementById('question');
       question.value = text.value;
 
-      // PythonのFlask APIを呼び出してコードを生成
+      // eelでPythonの関数を呼び出す
       eel.get_generated_code(text.value)(function(output) {
           let outputArea = document.getElementById('output');
           outputArea.value = output;
