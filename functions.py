b9da051326c95f13a299c74734994588ff9da731
ComonRaven <moonandstar118@gmail.com>
2024-12-24 22:29:51 +0900

    出力を調整

--- functions.py
+++ functions.py
@@ -8,7 +8,7 @@ client = OpenAI(api_key="APIKey")
 
 @eel.expose
 def get_generated_code(text):
-  response = client.chat.completions.create(model="gpt-4o-mini",
+  response = client.chat.completions.create(model="gpt-4o-mini", # モデルの指定 40-mini or 40
   messages=[
     {"role": "system", "content": "You are a helpful assistant."},
     {"role": "user", "content": text},
