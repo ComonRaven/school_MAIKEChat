d4ab7dfbd17fafe12ac4e2f74e755ecc198f47b3
ComonRaven <moonandstar118@gmail.com>
2025-01-15 13:39:49 +0900

    chatHistory.jsをChat.jsに統合

--- web/js/Chat.js
+++ web/js/Chat.js
@@ -102,3 +102,17 @@ function showExecute() {
 }
 
 document.onload = showChat();
+
+function showHistoryPanel() {
+    const historyPanel = document.getElementById("historyPanel");
+    const overlay = document.getElementById("historyOverlay");
+    historyPanel.classList.add("active");
+    overlay.classList.add("active"); // オーバーレイを表示
+}
+
+function closeHistoryPanel() {
+    const historyPanel = document.getElementById("historyPanel");
+    const overlay = document.getElementById("historyOverlay");
+    historyPanel.classList.remove("active");
+    overlay.classList.remove("active"); // オーバーレイを非表示
+}
